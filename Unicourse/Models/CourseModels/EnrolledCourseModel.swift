189e1b8aa9864f5a12629424b5fdbae1da8a10b5
13gucci <bibabibum0110@gmail.com>
2024-05-20 11:37:03 +0700

    📲 update models & continuing

---
+++ Unicourse/Models/CourseModels/EnrolledCourseModel.swift
@@ -0,0 +1,25 @@
+//
+//  EnrolledCourseModel.swift
+//  Unicourse
+//
+//  Created by Minh Trần Quang on 20/5/24.
+//
+
+import Foundation
+
+struct BasicUserInfo: Codable {
+    var _id: String
+    var email: String
+    var fullName: String
+    var profileName: String
+    var profile_image: String
+}
+
+struct EnrolledCourseModel: Codable {
+    var _id: String
+    var completed: Bool
+    var enrollDate: Date
+    var course: CourseModel
+    var progress: Int
+    var user: BasicUserInfo
+}
