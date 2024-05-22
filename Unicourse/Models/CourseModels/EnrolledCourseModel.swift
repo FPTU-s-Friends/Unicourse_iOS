5fee751d8e6838d4c2aedfa77bca28c3fbfc4e76
13gucci <bibabibum0110@gmail.com>
2024-05-22 17:55:49 +0700

    📲 modify structure model folder & files

--- Unicourse/Models/CourseModels/EnrolledCourseModel.swift
+++ Unicourse/Models/CourseModels/EnrolledCourseModel.swift
@@ -7,48 +7,7 @@
 
 import Foundation
 
-struct SubTrackId: Codable {
-    var _id: String
-    var title: String
-    var position: Int
-    var duration: Int
-    var content_url: String
-    var type: String
-}
 
-struct SubTrackProgress: Codable {
-    var _id: Int?
-    var subTrackId: SubTrackId
-    var completed: Bool
-}
-
-// Detail info videp của chapter
-struct TrackStep: Codable {
-    var _id: String
-    var title: String
-    var position: Int
-    var duration: Int
-    var content_url: String
-    var type: String
-}
-
-// Basic info video của chapter
-struct TrackId: Codable {
-    var _id: String
-    var position: Int
-    var chapterTitle: String
-    var track_steps: [TrackStep]
-}
-
-// Chapter của khoá học
-struct Track: Codable {
-    var _id: String
-    var trackId: TrackId
-    var completed: Bool
-    //  var subTrackProgress: [SubTrackProgress]
-    // Tạm thời cần ẩn đi subtrackprogress vì database cái có
-    // cái không
-}
 
 struct BasicUserInfo: Codable {
     var _id: String
@@ -62,7 +21,7 @@ struct BasicUserInfo: Codable {
 struct EnrolledCourseModel: Codable {
     var _id: String
     var user: BasicUserInfo
-    var course: CourseInEnrolledModel
+    var course: CourseInEnrolledCoursesModel
     var completed: Bool
     var enrollDate: String
     var progress: Int
