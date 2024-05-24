53e6960328f5922be032263bcb70027584692bfc
13gucci <bibabibum0110@gmail.com>
2024-05-24 17:52:24 +0700

    ss

--- Unicourse/Views/CommonUI/LoadingViews/LoadingIndicatorView.swift
+++ Unicourse/Views/CommonUI/LoadingViews/LoadingIndicatorView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct LoadingIndicatorView: View {
-    //comment
+    //commen t
     @State private var isAnimation = false
     @Binding var isLoading: Bool
     private let animationBackgroundColor = Color.mainColor1.opacity(0.3)
