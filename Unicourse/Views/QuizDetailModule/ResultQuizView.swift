410e991656433e6fc0a8d59e4e81691eb21b0914
13gucci <bibabibum0110@gmail.com>
2024-06-20 20:31:02 +0700

    📲 đang dở dang 9

---
+++ Unicourse/Views/QuizDetailModule/ResultQuizView.swift
@@ -0,0 +1,18 @@
+//
+//  ResultQuizView.swift
+//  Unicourse
+//
+//  Created by Minh Trần Quang on 20/6/24.
+//
+
+import SwiftUI
+
+struct ResultQuizView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResultQuizView()
+}
---
+++ Unicourse/Views/QuizDetailModule/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Unicourse
+//
+//  Created by Minh Trần Quang on 20/6/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
